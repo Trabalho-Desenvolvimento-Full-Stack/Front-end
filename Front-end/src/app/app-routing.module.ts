caa998bf0f47bf17eaf298bcac34e4ddac31652a
RainerWuttke <rainerzabel1@gmail.com>
2021-11-25 00:47:26 -0300

    Configuração básica de rotas

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EsqueceuSenhaComponent } from './pages/esqueceu-senha/esqueceu-senha.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'esqueceusenha', component: EsqueceuSenhaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
